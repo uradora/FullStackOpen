a6cbe8a3cd7991fd00b109c6bf498ecf4923c8a4
uradora <meri.rajamaki@gmail.com>
2021-06-20 14:34:21 +0300

    6.9 asti

---
+++ Osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const newAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(addAnecdote(content))
+  }
+
+  return (
+      <form onSubmit={newAnecdote}>
+        <input name='anecdote' />
+        <button type='submit'>create</button>
+      </form>
+  )
+}
+
+export default AnecdoteForm
---
+++ Osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addVote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return(
+    <div>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  return(
+      <ul>
+        {anecdotes.map(anecdote =>
+          <Anecdote
+            key={anecdote.id}
+            anecdote={anecdote}
+            handleClick={() =>
+              dispatch(addVote(anecdote.id))
+            }
+          />
+        )}
+      </ul>
+  )
+}
+
+export default AnecdoteList
